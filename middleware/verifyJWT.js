13282402448ae12a98e6b78a297ca394616ff4a7
esha <fariaesha2003@gmail.com>
2026-05-23 23:40:47 +0600

    fix verifyJWT cookies

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,8 +1,8 @@
 import jwt from "jsonwebtoken";
 
 const verifyJWT = (req, res, next) => {
-  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
-
+  
+const token = req.cookies?.token || req.headers.authorization?.split(" ")[1];
   if (!token) {
     return res.status(401).json({
       message: "Unauthorized Access"
